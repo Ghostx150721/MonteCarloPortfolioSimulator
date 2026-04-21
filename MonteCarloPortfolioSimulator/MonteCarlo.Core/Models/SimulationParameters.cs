ac046a39949816a17a4ba8e922d99982bc47b749
Hirusha Yohan <hirushayohanperera123@gmail.com>
2026-04-21 21:22:47 +0530

    feat: add simulation parameter and result models

---
+++ MonteCarloPortfolioSimulator/MonteCarlo.Core/Models/SimulationParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace MonteCarlo.Core.Models
+{
+    public class SimulationParameters
+    {
+        public double InitialInvestment { get; set; }
+        public double MonthlyContribution { get; set; }
+        public double MeanAnnualReturn { get; set; } // e.g. 0.08
+        public double Volatility { get; set; } // e.g. 0.15
+        public int Years { get; set; }
+        public int SimulationCount { get; set; }
+    }
+}
---
+++ MonteCarloPortfolioSimulator/MonteCarlo.Core/Models/SimulationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace MonteCarlo.Core.Models
+{
+    public class SimulationResult
+    {
+        public List<double> FinalPortfolioValues { get; set; } = new();
+        public double Average { get; set; }
+        public double Min { get; set; }
+        public double Max { get; set; }
+    }
+}
